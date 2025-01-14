cceab5b03149020992d365b76412e4d2f5489c0b
Doniyor <donish1979@gmail.com>
2025-01-15 00:19:11 +0300

    slight changes to develop branch as there were no changes here in order to pull request from feature/class. Sorry for that

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,2 +1,13 @@
-def test_init():
-    pass
+from src.main import Category
+
+
+def test_init_prod_samsung(product_samsung):
+    assert product_samsung.name == "Samsung"
+    assert product_samsung.price == 120000.0
+
+
+def test_init_category_notebooks(category_notebooks):
+    assert category_notebooks.description == "Ультрабуки, планшеты"
+    assert category_notebooks.products == ["ASUS", "SONY"]
+    Category.category_count = 0
+    Category.product_count = 0
