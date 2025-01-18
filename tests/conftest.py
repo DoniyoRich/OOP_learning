bde4381280459a48d8c6951980625eca5e67e76f
Doniyor <donish1979@gmail.com>
2025-01-18 12:44:44 +0300

    correction, removed unneccesary comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,13 +49,3 @@ def new_product_dict():
 @pytest.fixture(scope="function")
 def out_main():
     return "['Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\\n', 'Iphone 15, 210000.0 руб. Остаток: 8 шт.\\n', 'Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\\n']"
-
-
-# @pytest.fixture
-# def product():
-#     return Product
-#
-#
-# @pytest.fixture
-# def category():
-#     return Category
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,3 @@
-from itertools import product
-from unittest import mock
-
 from src.main import main
 from src.product import Product
 from src.category import Category
@@ -67,46 +64,7 @@ def test_add_product_to_category(category_notebooks, product_huawey):
     assert Category.product_count == 3
 
 
-# @patch(src.main.Category)
-# @patch(src.main.Product)
 def test_main(capsys, out_main):
     main()
     captured = capsys.readouterr()
     assert out_main in captured.out
-
-
-"""
-def test_if_main():
-    with mock.patch('__main__.__name__', '__main__'):
-        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-        category1 = Category(
-            "Смартфоны",
-            "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-            [product1, product2, product3]
-        )
-
-        print(category1.products)
-        product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-        category1.add_product(product4)
-        print(category1.products)
-        print(category1.product_count)
-
-        new_product = Product.new_product(
-            {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-             "quantity": 5})
-        print(new_product.name)
-        print(new_product.description)
-        print(new_product.price)
-        print(new_product.quantity)
-
-        new_product.price = 800
-        print(new_product.price)
-
-        new_product.price = -100
-        print(new_product.price)
-        new_product.price = 0
-        print(new_product.price)
-"""
